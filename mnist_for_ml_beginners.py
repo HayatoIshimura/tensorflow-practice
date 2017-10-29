dcb12a19b73efc1e2b8f67558620cddbc007dab6
Yoshiharu Yamada <yoshiharuseijin@me.com>
2017-10-29 21:45:22 +0900

    [update]: tensorboardで見れるようにした

--- mnist_for_ml_beginners.py
+++ mnist_for_ml_beginners.py
@@ -41,11 +41,20 @@ label = tf.placeholder(tf.float32, [None, 10], name="label-input")
 with tf.name_scope('xent') as scope:
     # 交差エントロピー
     cross_entropy = -tf.reduce_sum(label*tf.log(y))
-    ce_summ = tf.summary.scalar("cross entropy", cross_entropy)
+    tf.summary.scalar("cross entropy", cross_entropy)
 
 with tf.name_scope('train') as scope:
     # 最急降下法を使う
     train_gradient_decent = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+with tf.name_scope("test") as scope:
+    # 予測
+    with tf.name_scope("correct_prediction"):
+        correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(label,1))
+    with tf.name_scope('accuracy'):
+        # 精度の計算
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        tf.summary.scalar("accuracy", accuracy)
 
 # 全ての要約をマージしてそれらを /tmp/mnist_logs に書き出します。
 merged = tf.summary.merge_all()
@@ -57,24 +66,17 @@ init_op = tf.global_variables_initializer()
 # 変数の初期化セッションを実行する
 session.run(init_op)
 
-with tf.name_scope("test") as scope:
-    # 予測
-    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(label,1))
-    # 精度の計算
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    accuracy_summary = tf.summary.scalar("accuracy", accuracy)
-
 # ランダムミニバッチ学習を行う
 print("--- 訓練開始 ---")
 for epoch in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
     if epoch % 10 == 0:  # 要約データと正解率を記録します。
         result = session.run([merged, accuracy], feed_dict={x: mnist.test.images, label: mnist.test.labels})
         summary_str = result[0]
         acc = result[1]
-        writer.add_summary(summary_str, epoch)
+        writer.add_summary(result[0], epoch)
         print("Accuracy at step %s: %s" % (epoch, acc))
     else:
-        batch_xs, batch_ys = mnist.train.next_batch(100)
         session.run(train_gradient_decent, feed_dict={x: batch_xs, label: batch_ys})
 print("--- 訓練完了 ---")
 
