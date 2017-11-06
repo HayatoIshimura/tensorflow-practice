1f19d13a034080f22d64e59ca64cb88809ccc797
Yoshiharu Yamada <yoshiharuseijin@me.com>
2017-11-06 11:38:39 +0900

    [update]: train,valid,testで分けてデータを収集

--- eight_layer_nn.py
+++ eight_layer_nn.py
@@ -15,59 +15,90 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 print("--- MNISTデータの読み込み完了 ---")
 
 
-def affine(layer_data, next_layer_size, layer_number):
-    w = tf.Variable(tf.random_normal([layer_data.shape.as_list()[1], next_layer_size], mean=0.0, stddev=0.01))
-    b = tf.Variable(tf.zeros(next_layer_size))
-    z = tf.matmul(layer_data, w) + b
+def weight(height, width):
+    return tf.Variable(tf.random_normal([height, width], mean=0.0, stddev=0.01))
 
-    tf.summary.histogram("weight:" + str(layer_number), w)
-    tf.summary.histogram("bias:" + str(layer_number), b)
-    return z
+
+def bias(units):
+    return tf.Variable(tf.zeros(units))
+
+
+def affine(input_d, w, b):
+    return tf.matmul(input_d, w) + b
+
+
+def activation(input_d):
+    return tf.nn.relu(input_d)
 
 # InputLayer
-y_width = 10
+y_units = 10
 x = tf.placeholder(tf.float32, [None, 784])
 
 layer_input = x
 hidden_layer_size = 5
 
+w_hists = []
+b_hists = []
+a_hists = []
+
 for layer in range(1, hidden_layer_size):
-    next_layer_width = round((layer_input.shape.as_list()[1] + y_width) * 2 / 3)
+    next_layer_units = round((layer_input.shape.as_list()[1] + y_units) * 2 / 3)
 
-    with tf.name_scope("Wx_b") as scope:
-        z = affine(layer_input, next_layer_width, layer)
+    w = weight(layer_input.shape.as_list()[1], next_layer_units)
+    b = bias(next_layer_units)
 
-    with tf.name_scope("activation") as scope:
-        a = tf.nn.relu(z)
-        a_hist = tf.summary.histogram("activation:" + str(layer_input), a)
+    w_hists.append(tf.summary.histogram("weight:" + str(layer), w))
+    b_hists.append(tf.summary.histogram("bias:" + str(layer), b))
+
+    with tf.name_scope("Wx_b"):
+        z = affine(layer_input, w, b)
+
+    with tf.name_scope("activation"):
+        a = activation(z)
+        a_hists.append(tf.summary.histogram("activation:" + str(layer_input), a))
+
     layer_input = a
 
-with tf.name_scope("Wx_b") as scope:
-    z = affine(layer_input, y_width, hidden_layer_size)
+with tf.name_scope("Wx_b" + str(hidden_layer_size)):
+    w = weight(layer_input.shape.as_list()[1], y_units)
+    b = bias(y_units)
+    z = affine(layer_input, w, b)
+
+    w_hists.append(tf.summary.histogram("weight:" + str(hidden_layer_size + 1), w))
+    b_hists.append(tf.summary.histogram("bias:" + str(hidden_layer_size + 1), b))
 
 # name scope を使ってグラフ・ビジュアライザにおけるノードをまとめる。
-with tf.name_scope("output_layer") as scope:
+with tf.name_scope("output_layer"):
     # 出力層でソフトマックス回帰を実行
     y = tf.nn.softmax(z)
+    y_hist = tf.summary.histogram("y", y)
 
 
 # 正解ラベル
 label = tf.placeholder(tf.float32, [None, 10], name="label-input")
 
-with tf.name_scope('xent') as scope:
+with tf.name_scope('xent'):
     # 交差エントロピー
     cross_entropy = -tf.reduce_sum(label*tf.log(y))
     tf.summary.scalar("cross entropy", cross_entropy)
 
-with tf.name_scope('train') as scope:
-    # 最急降下法を使う
-    train_gradient_decent = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+# 精度計算
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(label,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-with tf.name_scope("test") as scope:
-    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(label,1))
-    # 精度の計算
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    tf.summary.scalar("accuracy", accuracy)
+with tf.name_scope("train"):
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    acc_summary_train = tf.summary.scalar("acc-train", accuracy)
+    loss_summary_train = tf.summary.scalar("cross_entropy_train", cross_entropy)
+
+with tf.name_scope("test"):
+    acc_summary_test = tf.summary.scalar("acc-test", accuracy)
+    loss_summary_test = tf.summary.scalar("cross_entropy_test", cross_entropy)
+
+with tf.name_scope("val"):
+    acc_summary_val = tf.summary.scalar("acc-val", accuracy)
+    loss_summary_val = tf.summary.scalar("cross_entropy_val", cross_entropy)
+
 
 # セッションを作成する。
 session = tf.Session()
@@ -75,7 +106,6 @@ session = tf.Session()
 # 全ての要約をマージしてそれらを /tmp/mnist_logs に書き出します。
 time_string = datetime.now().strftime('%Y%m%d/%H%M%S')
 
-merged = tf.summary.merge_all()
 writer = tf.summary.FileWriter("mnist_logs/" + time_string, session.graph_def)
 
 # 変数の初期化
@@ -89,14 +119,32 @@ print("--- 訓練開始 ---")
 for epoch in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
 
-    if epoch % 10 == 0:
-        result = session.run([merged, accuracy], feed_dict={x: mnist.test.images, label: mnist.test.labels})
-        summary_str = result[0]
-        acc = result[1]
-        writer.add_summary(summary_str, epoch)
-        print("Accuracy at epoch %s: %s" % (epoch, acc))
+    if epoch % 100 == 0:
+        train_list = [accuracy, acc_summary_train, loss_summary_train]
+        train_list.extend(w_hists)
+        train_list.extend(b_hists)
+        train_list.extend(a_hists)
+        train_list.append(y_hist)
+
+        result = session.run(train_list, feed_dict={x: batch_xs, label: batch_ys})
+        for i in range(1, len(result)):
+            writer.add_summary(result[i], epoch)
+        print("Train accuracy at epoch %s: %s" % (epoch, result[0]))
+
+        val_list = [accuracy, acc_summary_val, loss_summary_val]
+        result = session.run(val_list, feed_dict={x: mnist.validation.images, label: mnist.validation.labels})
+        for i in range(1, len(result)):
+            writer.add_summary(result[i], epoch)
+        print("Validation accuracy at epoch %s: %s" % (epoch, result[0]))
+
+        test_list = [accuracy, acc_summary_test, loss_summary_test]
+        result = session.run(test_list, feed_dict={x: mnist.test.images, label: mnist.test.labels})
+        for i in range(1, len(result)):
+            writer.add_summary(result[i], epoch)
+
+        print("Test accuracy at epoch %s: %s" % (epoch, result[0]))
     else:
-        result = session.run(train_gradient_decent, feed_dict={x: batch_xs, label: batch_ys})
+        result = session.run(train_step, feed_dict={x: batch_xs, label: batch_ys})
 print("--- 訓練完了 ---")
 
 # 終了時刻
